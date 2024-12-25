70a7bb2e7e291c4ec7e57fa355615f8dff251527
Thales Spanhol <thales_spanhol@hotmail.com>
2024-12-25 19:56:48 -0300

    edit js

--- script.js
+++ script.js
@@ -43,10 +43,6 @@ document.getElementById("pdfFile").addEventListener("change", function (event) {
         updateHTML("ctps", findAndReturnNextSeven("CTPS", textContent, 20));
         updateHTML("nome", findAndReturnNextSeven("Practice", textContent, 29));
         updateHTML(
-          "celular",
-          findAndReturnNextSeven("Telefone Celular ", textContent, 15)
-        );
-        updateHTML(
           "naturalidade",
           findAndReturnNextSeven("Naturalidade ", textContent, 12)
         );
